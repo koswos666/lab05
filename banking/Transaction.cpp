3bd382ecb3a8ef1383eb09d108b12cfd2b6230b6
koswos666 <kostikovassveta@gmail.com>
2025-05-21 15:51:16 +0300

    21

--- banking/Transaction.cpp
+++ banking/Transaction.cpp
@@ -1,3 +1,23 @@
+#include "Transaction.h"
+#include "Account.h"
+
+#include <cassert>
+#include <iostream>
+#include <stdexcept>
+
+namespace {
+struct Guard {
+  Guard(Account& account) : account_(&account) { account_->Lock(); }
+  ~Guard() { account_->Unlock(); }
+ private:
+  Account* account_;
+};
+}  
+
+Transaction::Transaction() : fee_(1) {}
+
+Transaction::~Transaction() {}
+
 bool Transaction::Make(Account& from, Account& to, int sum) {
   if (from.id() == to.id()) throw std::logic_error("invalid action");
   if (sum < 0) throw std::invalid_argument("sum can't be negative");
@@ -5,15 +25,36 @@ bool Transaction::Make(Account& from, Account& to, int sum) {
 
   Guard guard_from(from);
   Guard guard_to(to);
+
   if (from.GetBalance() < sum + fee_) {
     return false;
   }
+
   try {
     SaveToDataBase(from, to, sum);
   } catch (...) {
     return false;
   }
+
   Credit(to, sum);
   Debit(from, sum + fee_);
   return true;
+}
+
+void Transaction::Credit(Account& account, int sum) {
+  assert(sum > 0);
+  account.ChangeBalance(sum);
+}
+
+bool Transaction::Debit(Account& account, int sum) {
+  assert(sum > 0);
+  if (account.GetBalance() >= sum) {
+    account.ChangeBalance(-sum);
+    return true;
+  }
+  return false;
+}
+
+void Transaction::SaveToDataBase(Account& from, Account& to, int sum) {
+    std::cout << from.id() << " send to " << to.id() << " $" << sum << std::endl;
 }
