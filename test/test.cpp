99716031ab59facb11caab7a132476c10bc43a3c
koswos666 <kostikovassveta@gmail.com>
2025-05-21 17:56:15 +0300

    4

--- test/test.cpp
+++ test/test.cpp
@@ -17,41 +17,66 @@ TEST(AccountTest, GetBalance) {
     ASSERT_EQ(acc.GetBalance(), 100);
 }
 
+TEST(AccountTest, LockUnlock) {
+    Account acc(1, 200);
+    ASSERT_NO_THROW(acc.Lock());
+    ASSERT_THROW(acc.Lock(), std::runtime_error);
+    acc.Unlock();
+    ASSERT_NO_THROW(acc.Lock());
+}
+
 TEST(AccountTest, ChangeBalance) {
     Account acc(1, 100);
     ASSERT_THROW(acc.ChangeBalance(50), std::runtime_error);
     acc.Lock();
-    acc.ChangeBalance(50);
+    ASSERT_NO_THROW(acc.ChangeBalance(50));
     ASSERT_EQ(acc.GetBalance(), 150);
     acc.Unlock();
+    ASSERT_THROW(acc.ChangeBalance(-200), std::runtime_error);
 }
 
-TEST(TransactionTest, MakeTransaction) {
+TEST(TransactionTest, MakeValidTransaction) {
     NiceMock<MockTransaction> transaction;
     Account acc_from(1, 500);
     Account acc_to(2, 100);
     
-    ASSERT_THROW(transaction.Make(acc_from, acc_to, -50), std::invalid_argument);
-    ASSERT_THROW(transaction.Make(acc_from, acc_from, 100), std::logic_error);
-    
     EXPECT_CALL(transaction, SaveToDataBase(_, _, 100)).Times(1);
-    
     ASSERT_TRUE(transaction.Make(acc_from, acc_to, 100));
     ASSERT_EQ(acc_from.GetBalance(), 399);
     ASSERT_EQ(acc_to.GetBalance(), 200);
 }
 
-TEST(TransactionTest, FailedTransactionNotSaved) {
+TEST(TransactionTest, InvalidTransactions) {
+    NiceMock<MockTransaction> transaction;
+    Account acc1(1, 100);
+    Account acc2(2, 200);
+    
+    ASSERT_THROW(transaction.Make(acc1, acc2, -50), std::invalid_argument);
+    
+    ASSERT_THROW(transaction.Make(acc1, acc1, 100), std::logic_error);
+    
+    ASSERT_THROW(transaction.Make(acc1, acc2, 99), std::logic_error);
+}
+
+TEST(TransactionTest, InsufficientFunds) {
     NiceMock<MockTransaction> transaction;
     Account acc_from(1, 50);
     Account acc_to(2, 100);
     
     EXPECT_CALL(transaction, SaveToDataBase(_, _, _)).Times(0);
-    
     ASSERT_FALSE(transaction.Make(acc_from, acc_to, 100));
 }
 
-TEST(TransactionTest, DatabaseSaveFailure) {
+TEST(TransactionTest, DebitFailure) {
+    Transaction transaction;
+    Account acc(1, 50);
+    acc.Lock();
+  
+    ASSERT_FALSE(transaction.Debit(acc, 100));
+    ASSERT_EQ(acc.GetBalance(), 50);
+}
+
+TEST(TransactionTest, DatabaseErrorHandling) {
     MockTransaction transaction;
     Account acc_from(1, 500);
     Account acc_to(2, 100);
@@ -62,4 +87,18 @@ TEST(TransactionTest, DatabaseSaveFailure) {
     ASSERT_FALSE(transaction.Make(acc_from, acc_to, 100));
     ASSERT_EQ(acc_from.GetBalance(), 500);
     ASSERT_EQ(acc_to.GetBalance(), 100);
+}
+
+TEST(TransactionTest, CreditDebitOperations) {
+    Transaction transaction;
+    Account acc(1, 100);
+    acc.Lock();
+    
+    transaction.Credit(acc, 50);
+    ASSERT_EQ(acc.GetBalance(), 150);
+
+    ASSERT_TRUE(transaction.Debit(acc, 50));
+    ASSERT_EQ(acc.GetBalance(), 100);
+    
+    ASSERT_FALSE(transaction.Debit(acc, 150));
 }
