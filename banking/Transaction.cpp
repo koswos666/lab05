a0ea745b3146ae511ddda5313742a7cdaec1300f
koswos666 <kostikovassveta@gmail.com>
2025-05-21 20:08:46 +0300

    Update Transaction.cpp

--- banking/Transaction.cpp
+++ banking/Transaction.cpp
@@ -1,58 +1,57 @@
 #include "Transaction.h"
 #include "Account.h"
-
 #include <cassert>
 #include <iostream>
 #include <stdexcept>
 
 namespace {
-struct Guard {
-  Guard(Account& account) : account_(&account) { account_->Lock(); }
-  ~Guard() { account_->Unlock(); }
- private:
-  Account* account_;
-};
-}  
+    struct Guard {
+        Guard(Account& account) : account_(&account) { account_->Lock(); }
+        ~Guard() { account_->Unlock(); }
+    private:
+        Account* account_; // Исправлено с ^ на *
+    };
+}
 
 Transaction::Transaction() : fee_(1) {}
 
 Transaction::~Transaction() {}
 
-bool Transaction::Make(Account& from, Account& to, int sum) {
-  if (from.id() == to.id()) throw std::logic_error("invalid action");
-  if (sum < 0) throw std::invalid_argument("sum can't be negative");
-  if (sum < 100) throw std::logic_error("too small");
+bool Transaction::Make(Account& from, Account& to, int sum) { // Исправлено Accounts -> Account&
+    if (from.id() == to.id()) throw std::logic_error("invalid action");
+    if (sum < 0) throw std::invalid_argument("sum can't be negative");
+    if (sum < 100) throw std::logic_error("too small");
 
-  Guard guard_from(from);
-  Guard guard_to(to);
+    Guard guard_from(from);
+    Guard guard_to(to);
 
-  if (from.GetBalance() < sum + fee_) {
-    return false;
-  }
+    if (from.GetBalance() < sum + fee_) {
+        return false;
+    }
 
-  try {
-    SaveToDataBase(from, to, sum);
-  } catch (...) {
-    return false;
-  }
+    try {
+        SaveToDataBase(from, to, sum); // Исправлено SaveToDatabase -> SaveToDataBase
+    } catch (...) {
+        return false;
+    }
 
-  Credit(to, sum);
-  Debit(from, sum + fee_);
-  return true;
+    Credit(to, sum);
+    Debit(from, sum + fee_);
+    return true;
 }
 
 void Transaction::Credit(Account& account, int sum) {
-  assert(sum > 0);
-  account.ChangeBalance(sum);
+    assert(sum > 0);
+    account.ChangeBalance(sum);
 }
 
 bool Transaction::Debit(Account& account, int sum) {
-  assert(sum > 0);
-  if (account.GetBalance() >= sum) {
-    account.ChangeBalance(-sum);
-    return true;
-  }
-  return false;
+    assert(sum > 0);
+    if (account.GetBalance() >= sum) {
+        account.ChangeBalance(-sum);
+        return true;
+    }
+    return false;
 }
 
 void Transaction::SaveToDataBase(Account& from, Account& to, int sum) {
