ea21384cfae5bea923b244cfb186bbc73a040a34
koswos666 <kostikovassveta@gmail.com>
2025-05-22 16:05:47 +0300

    Update test.cpp 7

--- test/test.cpp
+++ test/test.cpp
@@ -12,63 +12,56 @@ public:
     bool TestDebit(Account& acc, int sum) { return Debit(acc, sum); }
 };
 
-TEST(AccountTest, ConstructorAndMethods) {
+TEST(AccountTest, ConstructorAndGetters) {
     Account acc(1, 500);
     ASSERT_EQ(acc.GetBalance(), 500);
     ASSERT_EQ(acc.id(), 1);
+}
 
-   
+TEST(AccountTest, LockUnlock) {
+    Account acc(2, 200);
     ASSERT_NO_THROW(acc.Lock());
-    ASSERT_THROW(acc.Lock(), std::runtime_error); 
+    ASSERT_THROW(acc.Lock(), std::runtime_error);
     acc.Unlock();
     ASSERT_NO_THROW(acc.Lock());
 }
 
-TEST(AccountTest, ChangeBalanceEdgeCases) {
-    Account acc(2, 200);
+TEST(AccountTest, ChangeBalance) {
+    Account acc(3, 300);
     
-
     ASSERT_THROW(acc.ChangeBalance(100), std::runtime_error);
     
     acc.Lock();
-
-    ASSERT_NO_THROW(acc.ChangeBalance(-50));
-    ASSERT_EQ(acc.GetBalance(), 150);
-    
     ASSERT_NO_THROW(acc.ChangeBalance(-200));
-    ASSERT_EQ(acc.GetBalance(), -50);
+    ASSERT_EQ(acc.GetBalance(), 100);
+    
+    acc.Unlock();
+    ASSERT_THROW(acc.ChangeBalance(-100), std::runtime_error);
 }
 
 TEST(TransactionTest, FullCoverage) {
     TransactionTestFriend tr;
-    Account acc1(1, 999);
+    Account acc1(1, 1000);
     Account acc2(2, 500);
-    ASSERT_THROW(tr.Make(acc1, acc1, 100), std::logic_error); 
-    ASSERT_THROW(tr.Make(acc1, acc2, -50), std::invalid_argument); 
-    ASSERT_THROW(tr.Make(acc1, acc2, 99), std::logic_error); 
 
-    ASSERT_FALSE(tr.Make(acc1, acc2, 999));
-    acc1.Lock(); 
-    acc1.ChangeBalance(1); 
-    acc1.Unlock(); 
-    ASSERT_TRUE(tr.Make(acc1, acc2, 500));
-    ASSERT_EQ(acc1.GetBalance(), 499);
-    ASSERT_EQ(acc2.GetBalance(), 1000);
+    ASSERT_THROW(tr.Make(acc1, acc1, 100), std::logic_error);
+    ASSERT_THROW(tr.Make(acc1, acc2, -50), std::invalid_argument);
+    ASSERT_THROW(tr.Make(acc1, acc2, 99), std::logic_error);
+
+    Account acc3(3, 500);
+    ASSERT_FALSE(tr.Make(acc3, acc2, 499));
+
+    ASSERT_TRUE(tr.Make(acc1, acc2, 400));
+    ASSERT_EQ(acc1.GetBalance(), 599);
+    ASSERT_EQ(acc2.GetBalance(), 900);
 }
 
- 
-
-TEST(TransactionTest, CreditDebitChecks) {
+TEST(TransactionTest, CreditDebitEdgeCases) {
     TransactionTestFriend tr;
     Account acc(1, 100);
     acc.Lock();
 
-   
-    tr.TestCredit(acc, 50);
-    ASSERT_EQ(acc.GetBalance(), 150);
-
+    ASSERT_DEATH(tr.TestCredit(acc, 0), ".*");
     ASSERT_TRUE(tr.TestDebit(acc, 50));
-    ASSERT_EQ(acc.GetBalance(), 100);
-
-    ASSERT_FALSE(tr.TestDebit(acc, 150));
+    ASSERT_FALSE(tr.TestDebit(acc, 100));
 }
