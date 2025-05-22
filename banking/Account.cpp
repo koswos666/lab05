879daf2409cb00d9c26c48cdf4cd4d95b1d21d2f
koswos666 <kostikovassveta@gmail.com>
2025-05-22 16:06:28 +0300

    Update Account.cpp 1

--- banking/Account.cpp
+++ banking/Account.cpp
@@ -1,22 +1,27 @@
 #include "Account.h"
-
 #include <stdexcept>
 
-Account::Account(int id, int balance)
+Account::Account(int id, int balance) 
     : id_(id), balance_(balance), is_locked_(false) {}
 
 Account::~Account() {}
 
-int Account::GetBalance() const { return balance_; }
+int Account::GetBalance() const { 
+    return balance_; 
+}
 
 void Account::ChangeBalance(int diff) {
-  if (!is_locked_) throw std::runtime_error("at first lock the account");
-  balance_ += diff;
+    if (!is_locked_) 
+        throw std::runtime_error("Lock first");
+    balance_ += diff;
 }
 
 void Account::Lock() {
-  if (is_locked_) throw std::runtime_error("already locked");
-  is_locked_ = true;
+    if (is_locked_) 
+        throw std::runtime_error("Already locked");
+    is_locked_ = true;
 }
 
-void Account::Unlock() { is_locked_ = false; }
+void Account::Unlock() { 
+    is_locked_ = false; 
+}
