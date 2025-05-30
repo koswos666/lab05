66398644386449748d35f270aa4bf4d33d4ad058
koswos666 <kostikovassveta@gmail.com>
2025-05-30 18:00:52 +0300

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -4,498 +4,141 @@
 #include "Transaction.h"
 
 using ::testing::_;
+using ::testing::Return;
 using ::testing::Throw;
+using ::testing::StrictMock;
+using ::testing::AtLeast;
 
-class TransactionTestFriend : public Transaction {
+
+class MockAccount : public Account {
 public:
-    void TestCredit(Account& acc, int sum) { Credit(acc, sum); }
-    bool TestDebit(Account& acc, int sum) { return Debit(acc, sum); }
+    MockAccount(int id, int balance) : Account(id, balance) {}
+    MOCK_METHOD(int, GetBalance, (), (const, override));
+    MOCK_METHOD(void, ChangeBalance, (int), (override));
+    MOCK_METHOD(void, Lock, (), (override));
+    MOCK_METHOD(void, Unlock, (), (override));
 };
 
-TEST(AccountTest, ConstructorAndMethods) {
-    Account acc(1, 500);
-    ASSERT_EQ(acc.GetBalance(), 500);
-    ASSERT_EQ(acc.id(), 1);
+// Мок-класс для Transaction (для перехвата SaveToDataBase)
+class MockTransaction : public Transaction {
+public:
+    MOCK_METHOD(void, SaveToDataBase, (Account& from, Account& to, int sum), (override));
+};
 
-   
-    ASSERT_NO_THROW(acc.Lock());
-    ASSERT_THROW(acc.Lock(), std::runtime_error); 
-    acc.Unlock();
-    ASSERT_NO_THROW(acc.Lock());
-}
-
-TEST(AccountTest, ChangeBalanceEdgeCases) {
-    Account acc(2, 200);
-    
-
-    ASSERT_THROW(acc.ChangeBalance(100), std::runtime_error);
+// Тесты для класса Account
+TEST(AccountTest, LockUnlockBehavior) {
+    Account acc(1, 100);
     
     acc.Lock();
-
-    ASSERT_NO_THROW(acc.ChangeBalance(-50));
-    ASSERT_EQ(acc.GetBalance(), 150);
+    EXPECT_NO_THROW(acc.ChangeBalance(50));
+    EXPECT_EQ(acc.GetBalance(), 150);
     
-    ASSERT_NO_THROW(acc.ChangeBalance(-200));
-    ASSERT_EQ(acc.GetBalance(), -50);
+    acc.Unlock();
+    EXPECT_THROW(acc.ChangeBalance(50), std::runtime_error);
 }
 
-TEST(TransactionTest, FullCoverage) {
-    TransactionTestFriend tr;
-    Account acc1(1, 999);
-    Account acc2(2, 500);
-
-   
-    ASSERT_THROW(tr.Make(acc1, acc1, 100), std::logic_error);
- 
-    ASSERT_NO_THROW(acc1.Lock()); 
-    acc1.Unlock(); 
-    
-    ASSERT_THROW(tr.Make(acc1, acc2, -50), std::invalid_argument);
-    ASSERT_NO_THROW(acc1.Lock());
-    acc1.Unlock();
-   
-
-    
-    ASSERT_THROW(tr.Make(acc1, acc2, 99), std::logic_error);
-    ASSERT_NO_THROW(acc1.Lock());
-    acc1.Unlock();
-    
-
-  
-    ASSERT_FALSE(tr.Make(acc1, acc2, 999));
-   
-    ASSERT_NO_THROW(acc1.Lock());
-    acc1.Unlock();
-    
-
-  
-    acc1.Lock();
-    acc1.ChangeBalance(1);
-    acc1.Unlock();
-
- 
-    ASSERT_TRUE(tr.Make(acc1, acc2, 500));
-    ASSERT_EQ(acc1.GetBalance(), 499); 
-    ASSERT_EQ(acc2.GetBalance(), 1000); 
-}
-TEST(TransactionTest, CreditDebitChecks) {
-    TransactionTestFriend tr;
+TEST(AccountTest, LockThrowsWhenAlreadyLocked) {
     Account acc(1, 100);
     acc.Lock();
-
-   
-    tr.TestCredit(acc, 50);
-    ASSERT_EQ(acc.GetBalance(), 150);
-
-    ASSERT_TRUE(tr.TestDebit(acc, 50));
-    ASSERT_EQ(acc.GetBalance(), 100);
-
-    ASSERT_FALSE(tr.TestDebit(acc, 150));
+    EXPECT_THROW(acc.Lock(), std::runtime_error);
 }
 
-TEST(TransactionTest, SaveToDatabaseException) {
+// Тесты для класса Transaction
+TEST(TransactionTest, MakeFailsOnInvalidAccounts) {
     Transaction tr;
-    Account acc1(1, 1000);
-    Account acc2(2, 500);
+    Account acc1(1, 100), acc2(1, 200);
     
-
-    class MockTransaction : public Transaction {
-    protected:
-        void SaveToDataBase(Account& from, Account& to, int sum) override {
-            throw std::runtime_error("DB error");
-        }
-    };
-
-    MockTransaction mockTr;
-    ASSERT_FALSE(mockTr.Make(acc1, acc2, 100));
+    EXPECT_THROW(tr.Make(acc1, acc2, 100), std::logic_error); // Same ID
+    EXPECT_THROW(tr.Make(acc1, Account(2, 200), -50), std::invalid_argument); // Negative sum
+    EXPECT_THROW(tr.Make(acc1, Account(2, 200), 99), std::logic_error); // Sum < 100
 }
 
-TEST(TransactionTest, SumBoundary) {
+TEST(TransactionTest, MakeFailsWhenInsufficientFunds) {
+    StrictMock<MockAccount> from(1, 100);
+    StrictMock<MockAccount> to(2, 0);
+    
+    EXPECT_CALL(from, Lock());
+    EXPECT_CALL(to, Lock());
+    EXPECT_CALL(from, GetBalance()).WillOnce(Return(100));
+    EXPECT_CALL(from, Unlock());
+    EXPECT_CALL(to, Unlock());
+    
     Transaction tr;
-    Account acc1(1, 200);
-    Account acc2(2, 0);
-
-    ASSERT_TRUE(tr.Make(acc1, acc2, 100));
-    ASSERT_EQ(acc1.GetBalance(), 99); 
-    ASSERT_EQ(acc2.GetBalance(), 100);
+    EXPECT_FALSE(tr.Make(from, to, 100)); // Нужно 100 + 1(fee) = 101
 }
 
-TEST(TransactionTest, DebitFailure) {
-    TransactionTestFriend tr;
-    Account acc(1, 50);
+TEST(TransactionTest, MakeSuccessfulTransaction) {
+    StrictMock<MockAccount> from(1, 200);
+    StrictMock<MockAccount> to(2, 0);
+    MockTransaction tr;
+    
+    // Ожидаемые вызовы
+    EXPECT_CALL(from, Lock());
+    EXPECT_CALL(to, Lock());
+    EXPECT_CALL(from, GetBalance()).WillOnce(Return(200));
+    EXPECT_CALL(tr, SaveToDataBase(_, _, 100));
+    EXPECT_CALL(to, ChangeBalance(100)); // Зачисление
+    EXPECT_CALL(from, ChangeBalance(-101)); // Списание + комиссия
+    EXPECT_CALL(from, Unlock());
+    EXPECT_CALL(to, Unlock());
+    
+    EXPECT_TRUE(tr.Make(from, to, 100));
+}
+
+TEST(TransactionTest, MakeFailsWhenDatabaseThrows) {
+    StrictMock<MockAccount> from(1, 200);
+    StrictMock<MockAccount> to(2, 0);
+    MockTransaction tr;
+    
+    EXPECT_CALL(from, Lock());
+    EXPECT_CALL(to, Lock());
+    EXPECT_CALL(from, GetBalance()).WillOnce(Return(200));
+    EXPECT_CALL(tr, SaveToDataBase(_, _, _)).WillOnce(Throw(std::runtime_error("DB Error")));
+    EXPECT_CALL(from, Unlock());
+    EXPECT_CALL(to, Unlock());
+    
+    EXPECT_FALSE(tr.Make(from, to, 100));
+}
+
+// Тестирование приватных методов через дружественный класс
+class TransactionTestFriend {
+public:
+    static void Credit(Transaction& tr, Account& acc, int sum) {
+        tr.Credit(acc, sum);
+    }
+    static bool Debit(Transaction& tr, Account& acc, int sum) {
+        return tr.Debit(acc, sum);
+    }
+};
+
+TEST(TransactionTest, CreditAndDebitOperations) {
+    Transaction tr;
+    Account acc(1, 100);
     acc.Lock();
     
-    ASSERT_FALSE(tr.TestDebit(acc, 100));
-    ASSERT_EQ(acc.GetBalance(), 50);
-}
-
-TEST(TransactionTest, GuardUnlocksOnException) {
-    Transaction tr;
-    Account acc1(1, 1000);
-    Account acc2(2, 500);
-
-    try {
-        tr.Make(acc1, acc2, 99);
-    } catch (...) {}
-
-    ASSERT_NO_THROW(acc1.Lock()); 
-    ASSERT_NO_THROW(acc2.Lock());
+    // Тест Credit
+    TransactionTestFriend::Credit(tr, acc, 50);
+    EXPECT_EQ(acc.GetBalance(), 150);
+    
+    // Тест Debit (успех)
+    EXPECT_TRUE(TransactionTestFriend::Debit(tr, acc, 50));
+    EXPECT_EQ(acc.GetBalance(), 100);
+    
+    // Тест Debit (недостаточно средств)
+    EXPECT_FALSE(TransactionTestFriend::Debit(tr, acc, 150));
+    
+    // Тест исключений
+    EXPECT_THROW(TransactionTestFriend::Credit(tr, acc, -10), std::invalid_argument);
+    EXPECT_THROW(TransactionTestFriend::Debit(tr, acc, -10), std::invalid_argument);
 }
 
 TEST(TransactionTest, FeeConfiguration) {
     Transaction tr;
     tr.set_fee(10);
-    ASSERT_EQ(tr.fee(), 10);
+    EXPECT_EQ(tr.fee(), 10);
 }
 
-
-TEST(TransactionTest, DebitInsufficientBalance) {
-    TransactionTestFriend tr;
-    Account acc(1, 50);
-    acc.Lock();
-    
-    ASSERT_FALSE(tr.TestDebit(acc, 100));
-    ASSERT_EQ(acc.GetBalance(), 50);
-}
-
-TEST(AccountTest, ChangeBalanceWithoutLock) {
-    Account acc(1, 200);
-    ASSERT_THROW(acc.ChangeBalance(100), std::runtime_error); 
-}
-
-TEST(TransactionTest, InsufficientBalance) {
-    Transaction tr;
-    Account acc1(1, 100);
-    Account acc2(2, 0);
-
-    ASSERT_FALSE(tr.Make(acc1, acc2, 100));
-}
-
-TEST(TransactionTest, DatabaseExceptionHandling) {
-    class MockTransaction : public Transaction {
-    protected:
-        void SaveToDataBase(Account& from, Account& to, int sum) override {
-            throw std::runtime_error("DB error");
-        }
-    };
-
-    MockTransaction mockTr;
-    Account acc1(1, 1000);
-    Account acc2(2, 500);
-
-    ASSERT_FALSE(mockTr.Make(acc1, acc2, 100));
-}
-TEST(AccountTest, DoubleLockThrows) {
-    Account acc(1, 100);
-    acc.Lock();
-    ASSERT_THROW(acc.Lock(), std::runtime_error);
-}
-TEST(TransactionTest, SameAccountIdsThrow) {
-    Transaction tr;
-    Account acc(1, 500);
-    ASSERT_THROW(tr.Make(acc, acc, 100), std::logic_error); 
-}
-TEST(TransactionTest, SmallSumThrow) {
-    Transaction tr;
-    Account acc1(1, 200);
-    Account acc2(2, 0);
-    ASSERT_THROW(tr.Make(acc1, acc2, 99), std::logic_error); 
-}
-
-TEST(TransactionTest, DebitFailsWhenLowBalance) {
-    TransactionTestFriend tr;
-    Account acc(1, 50);
-    acc.Lock();
-    ASSERT_FALSE(tr.TestDebit(acc, 100)); 
-    ASSERT_EQ(acc.GetBalance(), 50);
-}
-TEST(TransactionTest, CreditZeroSumThrows) {
-    TransactionTestFriend tr;
-    Account acc(1, 100);
-    acc.Lock();
-    ASSERT_THROW(tr.TestCredit(acc, 0), std::invalid_argument);
-}
-
-
-TEST(TransactionTest, SaveToDatabaseCalledCorrectly) {
-    class MockTransaction : public Transaction {
-    protected:
-        void SaveToDataBase(Account& from, Account& to, int sum) override {
-            called = true;
-            Transaction::SaveToDataBase(from, to, sum);
-        }
-    public:
-        bool called = false;
-    };
-
-    MockTransaction mockTr;
-    Account acc1(1, 1000);
-    Account acc2(2, 500);
-    
-    ASSERT_TRUE(mockTr.Make(acc1, acc2, 100));
-    ASSERT_TRUE(mockTr.called);
-}
-TEST(TransactionTest, GuardUnlocksOnDatabaseException) {
-    class MockTransaction : public Transaction {
-    protected:
-        void SaveToDataBase(Account& from, Account& to, int sum) override {
-            throw std::runtime_error("DB error");
-        }
-    };
-
-    MockTransaction mockTr;
-    Account acc1(1, 1000);
-    Account acc2(2, 500);
-    
-    ASSERT_FALSE(mockTr.Make(acc1, acc2, 100));
-    ASSERT_NO_THROW(acc1.Lock()); 
-    ASSERT_NO_THROW(acc2.Lock());
-}
-TEST(TransactionTest, DebitEdgeCaseBalanceEqualsSum) {
-    TransactionTestFriend tr;
-    Account acc(1, 100);
-    acc.Lock();
-    
-    ASSERT_TRUE(tr.TestDebit(acc, 100));
-    ASSERT_EQ(acc.GetBalance(), 0);
-}
-
-
-
-TEST(TransactionTest, DebitZeroSumThrows) {
-    TransactionTestFriend tr;
-    Account acc(1, 100);
-    acc.Lock();
-    ASSERT_THROW(tr.TestDebit(acc, 0), std::invalid_argument);
-}
-
-
-TEST(AccountTest, UnlockDoesNotThrowWhenUnlocked) {
-    Account acc(1, 100);
-    ASSERT_NO_THROW(acc.Unlock());
-}
-TEST(AccountTest, GetBalanceReturnsCorrectValue) {
-    Account acc(1, 200);
-    ASSERT_EQ(acc.GetBalance(), 200);
-}
-TEST(TransactionTest, BalanceExactlySumPlusFee) {
-    Transaction tr;
-    Account acc1(1, 101); 
-    Account acc2(2, 0);
-    ASSERT_TRUE(tr.Make(acc1, acc2, 100));
-    ASSERT_EQ(acc1.GetBalance(), 0);
-}
-TEST(TransactionTest, GuardUnlockOrder) {
-    class MockAccount : public Account {
-    public:
-        MockAccount(int id, int balance) : Account(id, balance) {}
-        void Unlock() override {
-            static bool toUnlocked = false;
-            if (id() == 2) toUnlocked = true;
-            if (id() == 1 && !toUnlocked) {
-                ADD_FAILURE() << "from unlocked before to!";
-            }
-            Account::Unlock();
-        }
-    };
-
-    MockAccount acc1(1, 1000);
-    MockAccount acc2(2, 500);
-    Transaction tr;
-    tr.Make(acc1, acc2, 100);
-}
-TEST(TransactionTest, FeeGetter) {
-    Transaction tr;
-    ASSERT_EQ(tr.fee(), 1);
-}
-TEST(TransactionTest, SaveToDatabaseOutput) {
-    testing::internal::CaptureStdout();
-    Transaction tr;
-    Account acc1(1, 1000);
-    Account acc2(2, 500);
-    tr.Make(acc1, acc2, 100);
-    std::string output = testing::internal::GetCapturedStdout();
-    ASSERT_EQ(output, "1 send to 2 $100\n");
-}
-
-TEST(TransactionTest, GuardUnlocksOnInsufficientBalance) {
-    Transaction tr;
-    Account acc1(1, 100); 
-    Account acc2(2, 0);
-
-    ASSERT_FALSE(tr.Make(acc1, acc2, 100));
-    ASSERT_NO_THROW(acc1.Lock());
-    ASSERT_NO_THROW(acc2.Lock());
-    acc1.Unlock();
-    acc2.Unlock();
-}
-
-TEST(TransactionTest, GuardUnlocksOnSuccess) {
-    Transaction tr;
-    Account acc1(1, 200);
-    Account acc2(2, 0);
-
-    ASSERT_TRUE(tr.Make(acc1, acc2, 100));
-    ASSERT_NO_THROW(acc1.Lock());
-    ASSERT_NO_THROW(acc2.Lock());
-    acc1.Unlock();
-    acc2.Unlock();
-}
-
-
-TEST(TransactionTest, CreditDebitInvalidSum) {
-    TransactionTestFriend tr;
-    Account acc(1, 100);
-    acc.Lock();
-    
-    ASSERT_THROW(tr.TestCredit(acc, -50), std::invalid_argument);
-    ASSERT_THROW(tr.TestDebit(acc, -50), std::invalid_argument);
-    ASSERT_THROW(tr.TestCredit(acc, 0), std::invalid_argument);
-    ASSERT_THROW(tr.TestDebit(acc, 0), std::invalid_argument);
-}
-
-
-TEST(AccountTest, ChangeBalanceThrowsWhenNotLocked) {
-    Account acc(1, 100);
-    ASSERT_THROW(acc.ChangeBalance(50), std::runtime_error);
-}
-
-TEST(AccountTest, ConstructorInitialization) {
-    Account acc(42, 999);
-    ASSERT_EQ(acc.id(), 42);
-    ASSERT_EQ(acc.GetBalance(), 999);
-}
-TEST(TransactionTest, NegativeCredit) {
-    TransactionTestFriend tr;
-    Account acc(1, 100);
-    acc.Lock();
-    ASSERT_THROW(tr.TestCredit(acc, -50), std::invalid_argument);
-}
-
-TEST(TransactionTest, NegativeDebit) {
-    TransactionTestFriend tr;
-    Account acc(1, 100);
-    acc.Lock();
-    ASSERT_THROW(tr.TestDebit(acc, -50), std::invalid_argument);
-}
-
-TEST(TransactionTest, UnlockAfterException) {
-    Account acc(1, 100);
-    ASSERT_NO_THROW(acc.Unlock());
-}
-TEST(TransactionTest, SaveToDatabaseInSuccessCase) {
-    class MockTransaction : public Transaction {
-    public:
-        bool saveCalled = false;
-    protected:
-        void SaveToDataBase(Account& from, Account& to, int sum) override {
-            saveCalled = true;
-            Transaction::SaveToDataBase(from, to, sum);
-        }
-    };
-
-    MockTransaction mockTr;
-    Account acc1(1, 1000);
-    Account acc2(2, 500);
-    
-    ASSERT_TRUE(mockTr.Make(acc1, acc2, 100));
-    ASSERT_TRUE(mockTr.saveCalled);
-}
-
-
-TEST(TransactionTest, FeeChangeAffectsTransaction) {
-    Transaction tr;
-    tr.set_fee(5);
-    Account acc1(1, 200);
-    Account acc2(2, 0);
-
-    ASSERT_TRUE(tr.Make(acc1, acc2, 100));
-    ASSERT_EQ(acc1.GetBalance(), 95);
-}
-TEST(AccountTest, LockUnlockSequence) {
-    Account acc(1, 100);
-    acc.Lock();
-    acc.Unlock();
-    ASSERT_NO_THROW(acc.Lock());
-}
-
-TEST(TransactionTest, DebitExactBalanceAfterFee) {
-    TransactionTestFriend tr;
-    Account acc(1, 101);
-    acc.Lock();
-    ASSERT_TRUE(tr.TestDebit(acc, 101));
-    ASSERT_EQ(acc.GetBalance(), 0);
-}
-
-TEST(TransactionTest, CreditAndDebitWithHighValues) {
-    TransactionTestFriend tr;
-    Account acc(1, 1000000);
-    acc.Lock();
-    tr.TestCredit(acc, 500000);
-    ASSERT_EQ(acc.GetBalance(), 1500000);
-    ASSERT_TRUE(tr.TestDebit(acc, 1000000));
-    ASSERT_EQ(acc.GetBalance(), 500000);
-}
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
-}
-// Тест на разблокировку при исключении в конструкторе Guard
-TEST(TransactionTest, GuardUnlocksOnConstructorException) {
-    class MockAccount : public Account {
-    public:
-        MockAccount(int id, int balance) : Account(id, balance) {}
-        void Lock() override { throw std::runtime_error("Lock failed"); }
-    };
-
-    MockAccount acc1(1, 1000);
-    Account acc2(2, 500);
-    Transaction tr;
-
-    ASSERT_THROW(tr.Make(acc1, acc2, 100), std::runtime_error);
-    ASSERT_NO_THROW(acc2.Lock()); // Проверяем что второй аккаунт разблокирован
-}
-
-
-TEST(TransactionTest, ZeroFeeTransaction) {
-    Transaction tr;
-    tr.set_fee(0);
-    
-    Account acc1(1, 100);
-    Account acc2(2, 0);
-    
-    ASSERT_TRUE(tr.Make(acc1, acc2, 100));
-    ASSERT_EQ(acc1.GetBalance(), 0);
-    ASSERT_EQ(acc2.GetBalance(), 100);
-}
-
-// Тест на максимально возможную сумму транзакции
-TEST(TransactionTest, MaxIntTransaction) {
-    Transaction tr;
-    Account acc1(1, INT_MAX);
-    Account acc2(2, 0);
-    
-    ASSERT_TRUE(tr.Make(acc1, acc2, INT_MAX - tr.fee()));
-    ASSERT_EQ(acc1.GetBalance(), 0);
-    ASSERT_EQ(acc2.GetBalance(), INT_MAX - tr.fee());
-}
-
-TEST(TransactionTest, SaveToDatabaseExceptionDoesNotLog) {
-    class MockTransaction : public Transaction {
-    protected:
-        void SaveToDataBase(Account& from, Account& to, int sum) override {
-            throw std::runtime_error("DB connection failed");
-        }
-    };
-
-    MockTransaction mockTr;
-    Account acc1(1, 1000);
-    Account acc2(2, 500);
-    
-    testing::internal::CaptureStderr();
-    ASSERT_FALSE(mockTr.Make(acc1, acc2, 100));
-    std::string output = testing::internal::GetCapturedStderr();
-    
-    // Исключение перехватывается внутри Make, поэтому в stderr ничего не должно быть
-    ASSERT_TRUE(output.empty()) << "Expected empty stderr, but got: " << output;
 }
