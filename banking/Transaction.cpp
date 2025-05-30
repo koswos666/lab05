5669953066479f68777116c7051a2d92035fcb76
koswos666 <kostikovassveta@gmail.com>
2025-05-30 18:48:40 +0300

    Update Transaction.cpp

--- banking/Transaction.cpp
+++ banking/Transaction.cpp
@@ -1,59 +1,55 @@
 #include "Transaction.h"
 #include "Account.h"
+
 #include <cassert>
 #include <iostream>
 #include <stdexcept>
 
 namespace {
     struct Guard {
-        Guard(Account& account) : account_(account) { account_.Lock(); }
-        ~Guard() { account_.Unlock(); }
+        Guard(Account& account) : account_(&account) { account_->Lock(); }
+        ~Guard() { account_->Unlock(); }
     private:
-        Account& account_;
+        Account* account_;
     };
-}
+} // namespace
 
 Transaction::Transaction() : fee_(1) {}
-
-Transaction::~Transaction() {}
+Transaction::~Transaction() = default;
 
 bool Transaction::Make(Account& from, Account& to, int sum) {
     if (from.id() == to.id()) throw std::logic_error("invalid action");
     if (sum < 0) throw std::invalid_argument("sum can't be negative");
+    if (fee_ * 2 > sum) return false;
     if (sum < 100) throw std::logic_error("too small");
 
-    Guard guard_from(from);
-    Guard guard_to(to);
+    { // Блок с Guard
+        Guard guard_from(from);
+        Guard guard_to(to);
 
-    if (from.GetBalance() < sum + fee_) {
-        return false;
-    }
+        if (!Debit(from, sum + fee_)) {
+            return false;
+        }
+        Credit(to, sum);
+    } // Конец блока
 
-    try {
-        SaveToDataBase(from, to, sum);
-    } catch (...) {
-        return false;
-    }
-
-    Credit(to, sum);
-    Debit(from, sum + fee_);
+    SaveToDataBase(from, to, sum);
     return true;
 }
 
 void Transaction::Credit(Account& account, int sum) {
-    if (sum <= 0) throw std::invalid_argument("Credit sum must be positive");
     account.ChangeBalance(sum);
 }
 
 bool Transaction::Debit(Account& account, int sum) {
-    if (sum <= 0) throw std::invalid_argument("Debit sum must be positive");
-    if (account.GetBalance() < sum) {
-        return false;
+    if (account.GetBalance() >= sum) {
+        account.ChangeBalance(-sum);
+        return true;
     }
-    account.ChangeBalance(-sum);
-    return true;
+    return false;
 }
-
 void Transaction::SaveToDataBase(Account& from, Account& to, int sum) {
-    std::cout << from.id() << " send to " << to.id() << " $" << sum << "\n";
+  std::cout << from.id() << " send to " << to.id() << " $" << sum << std::endl;
+  std::cout << "Balance " << from.id() << " is " << from.GetBalance() << std::endl;
+  std::cout << "Balance " << to.id() << " is " << to.GetBalance() << std::endl;
 }
