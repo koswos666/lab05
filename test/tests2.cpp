6e2935662701adc220fb71b17f88e489b17c085a
koswos666 <kostikovassveta@gmail.com>
2025-05-30 18:41:45 +0300

    Create tests2.cpp

---
+++ test/tests2.cpp
@@ -0,0 +1,31 @@
+#include <gtest/gtest.h>
+#include "Account.h"
+
+TEST(AccountTest, InitialBalanceIsCorrect) {
+    Account acc(1, 1000);
+    ASSERT_EQ(acc.GetBalance(), 1000);
+}
+
+TEST(AccountTest, BalanceChangeRequiresLock) {
+    Account acc(1, 1000);
+    ASSERT_THROW(acc.ChangeBalance(100), std::runtime_error);
+    acc.Lock();
+    ASSERT_NO_THROW(acc.ChangeBalance(100));
+    ASSERT_EQ(acc.GetBalance(), 1100);
+}
+
+TEST(AccountTest, DoubleLockThrowsException) {
+    Account acc(1, 100);
+    acc.Lock();
+    ASSERT_THROW(acc.Lock(), std::runtime_error);
+}
+
+TEST(AccountTest, UnlockUnlockedAccountSucceeds) {
+    Account acc(1, 100);
+    ASSERT_NO_THROW(acc.Unlock());
+}
+
+TEST(AccountTest, UnlockedBalanceChangeThrows) {
+    Account acc(1, 100);
+    ASSERT_THROW(acc.ChangeBalance(50), std::runtime_error);
+}
