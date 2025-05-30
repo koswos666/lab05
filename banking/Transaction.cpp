01abbee277e208b7496443fb3082e40b0b008f8e
koswos666 <kostikovassveta@gmail.com>
2025-05-30 13:34:28 +0300

    Update Transaction.cpp

--- banking/Transaction.cpp
+++ banking/Transaction.cpp
@@ -6,10 +6,10 @@
 
 namespace {
     struct Guard {
-        Guard(Account& account) : account_(&account) { account_->Lock(); }
-        ~Guard() { account_->Unlock(); }
+        Guard(Account& account) : account_(account) { account_.Lock(); }
+        ~Guard() { account_.Unlock(); }
     private:
-        Account* account_; 
+        Account& account_;
     };
 }
 
@@ -17,7 +17,7 @@ Transaction::Transaction() : fee_(1) {}
 
 Transaction::~Transaction() {}
 
-bool Transaction::Make(Account& from, Account& to, int sum) { 
+bool Transaction::Make(Account& from, Account& to, int sum) {
     if (from.id() == to.id()) throw std::logic_error("invalid action");
     if (sum < 0) throw std::invalid_argument("sum can't be negative");
     if (sum < 100) throw std::logic_error("too small");
@@ -30,7 +30,7 @@ bool Transaction::Make(Account& from, Account& to, int sum) {
     }
 
     try {
-        SaveToDataBase(from, to, sum); 
+        SaveToDataBase(from, to, sum);
     } catch (...) {
         return false;
     }
@@ -41,19 +41,19 @@ bool Transaction::Make(Account& from, Account& to, int sum) {
 }
 
 void Transaction::Credit(Account& account, int sum) {
-    assert(sum > 0);
+    if (sum <= 0) throw std::invalid_argument("Credit sum must be positive");
     account.ChangeBalance(sum);
 }
 
 bool Transaction::Debit(Account& account, int sum) {
-    assert(sum > 0);
-    if (account.GetBalance() >= sum) {
-        account.ChangeBalance(-sum);
-        return true;
+    if (sum <= 0) throw std::invalid_argument("Debit sum must be positive");
+    if (account.GetBalance() < sum) {
+        return false;
     }
-    return false;
+    account.ChangeBalance(-sum);
+    return true;
 }
 
 void Transaction::SaveToDataBase(Account& from, Account& to, int sum) {
-    std::cout << from.id() << " send to " << to.id() << " $" << sum << std::endl;
+    std::cout << from.id() << " send to " << to.id() << " $" << sum << "\n";
 }
